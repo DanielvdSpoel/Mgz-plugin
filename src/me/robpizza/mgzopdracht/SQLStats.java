11b3b310912e470ccaa1ee826000879ea6b7414f
Rob <Robpizza@users.noreply.github.com>
2017-04-15 16:27:37 +0200

    Update SQLStats.java

--- src/me/robpizza/mgzopdracht/SQLStats.java
+++ src/me/robpizza/mgzopdracht/SQLStats.java
@@ -5,106 +5,58 @@ import java.sql.SQLException;
 
 public class SQLStats {
 
-	public static boolean playerExists(String name) {
+	public static void createPlayer(String name) {
+		Main.mysql.update("INSERT INTO players(name, kills, deaths) VALUES ('" + name + "', '0', '0') ON DUPLICATE KEY UPDATE name=name;");
+	}
 
+
+	public static Integer getKills(String name) {
+		Integer i = 0;
 		try {
 			ResultSet rs = Main.mysql.query("SELECT * FROM players WHERE name= '" + name + "'");
+			if((!rs.next()) || (Integer.valueOf(rs.getInt("KILLS")) == null)) {
+				createPlayer(name);
+				getKills(name);
+			}
 
-			if(rs.next()) {
+			i = rs.getInt("KILLS");
 
-				return rs.getString("name") != null;
-			} 
-			return false;
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		return false;
-
-	}
-	
-	public static void createPlayer(String name) {
-		
-		if(!(playerExists(name))) {
-			Main.mysql.update("INSERT INTO players(name, kills, deaths) VALUES ('" + name + "', '0', '0');");
-		}
-	}
-	
-	public static Integer getKills(String name) {
-		Integer i = 0;
-		
-		if(playerExists(name)) {
-			try {
-				ResultSet rs = Main.mysql.query("SELECT * FROM players WHERE name= '" + name + "'");
-				if((!rs.next()) || (Integer.valueOf(rs.getInt("KILLS")) == null));
-				
-				i = rs.getInt("KILLS");
-				
-			}catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}  else {
-			createPlayer(name);
-			getKills(name);
-		}
-		
 		return i;
 	}
-	
+
 	public static Integer getDeaths(String name) {
 		Integer i = 0;
-		
-		if(playerExists(name)) {
-			try {
-				ResultSet rs = Main.mysql.query("SELECT * FROM players WHERE name= '" + name + "'");
-				if((!rs.next()) || (Integer.valueOf(rs.getInt("DEATHS")) == null));
-				
-				i = rs.getInt("DEATHS");
-				
-			}catch(SQLException e) {
-				e.printStackTrace();
+		try {
+			ResultSet rs = Main.mysql.query("SELECT * FROM players WHERE name= '" + name + "'");
+			if((!rs.next()) || (Integer.valueOf(rs.getInt("DEATHS")) == null)) {
+				createPlayer(name);
+				getDeaths(name);
 			}
-		}  else {
-			createPlayer(name);
-			getDeaths(name);
+
+			i = rs.getInt("DEATHS");
+
+		}catch(SQLException e) {
+			e.printStackTrace();
 		}
-		
 		return i;
 	}
-	
+
 	public static void setKills(String name, Integer kills) {
-		if(playerExists(name)) {
-			Main.mysql.update("UPDATE players SET KILLS= '" + kills + "' WHERE name='" + name + "';");
-		} else {
-			createPlayer(name);
-			setKills(name, kills);
-		}
+		Main.mysql.update("UPDATE players SET KILLS= '" + kills + "' WHERE name='" + name + "';");
 	}
-	
+
 	public static void setDeaths(String name, Integer deaths) {
-		if(playerExists(name)) {
-			Main.mysql.update("UPDATE players SET DEATHS= '" + deaths + "' WHERE name='" + name + "';");
-		} else {
-			createPlayer(name);
-			setDeaths(name, deaths);
-		}
+		Main.mysql.update("UPDATE players SET DEATHS= '" + deaths + "' WHERE name='" + name + "';");
 	}
-	
+
 	public static void addKills(String name, Integer kills) {
-		if(playerExists(name)) {
-			setKills(name, Integer.valueOf(getKills(name).intValue() + kills.intValue()));
-		} else {
-			createPlayer(name);
-			addKills(name, kills);
-		}
+		setKills(name, Integer.valueOf(getKills(name).intValue() + kills.intValue()));
 	}
-	
+
 	public static void addDeaths(String name, Integer deaths) {
-		if(playerExists(name)) {
-			setDeaths(name, Integer.valueOf(getDeaths(name).intValue() + deaths.intValue()));
-		} else {
-			createPlayer(name);
-			addDeaths(name, deaths);
-		}
+		setDeaths(name, Integer.valueOf(getDeaths(name).intValue() + deaths.intValue()));
 	}
-	
 }
